9fb1deb5b9b1dcb1e678d6399e75c0c144823bda
Matthew Rosenberg <mateo@Matthews-MacBook-Pro.local>
2019-01-12 18:42:00 -0700

    Added an export module

--- App.js
+++ App.js
@@ -2,14 +2,21 @@ console.log("Starting App")
 
 const fs = require('fs')
 const os = require('os')
+const _ = require("lodash")
 const notes = require("./notes.js")
 
 var result = notes.add(1,2)
 console.log(result)
 
+console.log(_.isString(true))
+console.log(_.isString("Andrew"))
 
-// var user = os.userInfo()
+var filteredArray = _.uniq(["Andrew", 1, "Andrew", 1, 2, 3, 4])
+console.log(filteredArray)
 
-// fs.appendFile("greetings.txt", `Hello ${user.username}!  You are ${notes.age}`, function(err) {
-//   if(err) "unable to write file"
-// })
+
+var user = os.userInfo()
+
+fs.appendFile("greetings.txt", `Hello ${user.username}!  You are ${notes.age}`, function(err) {
+  if(err) "unable to write file"
+})
